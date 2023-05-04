76df7f40269d134e8137781cf8c3138504703522
Lucca Ambrosius <ismartlucca@gmail.com>
2023-05-04 19:54:02 -0300

    connecting to mongo and creating user

---
+++ src/database/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    console.log("Waiting for connection...")
+
+    mongoose.connect("mongodb+srv://luccaambrosiuswk:12312344@cluster0.aitu9fc.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB atlas sucessfully connected")).catch((error) => console.log(error))
+};
+
+module.exports = connectDatabase;
