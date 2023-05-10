fa30cc2e6ccb94ce7098ee3b26072aa7e08ad4f5
Lucca Ambrosius <ismartlucca@gmail.com>
2023-05-10 05:17:28 -0300

    using try and catch

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -2,17 +2,19 @@ const mongoose = require("mongoose");
 const userService = require("../services/user.service");
 
 const validId = (req, res, next) => {
-  const id = req.params.id;
+  try{const id = req.params.id;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).send({ message: "Invalid ID" });
   }
 
-  next();
+  next();} catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
 const validUser = async (req, res, next) => {
-  const id = req.params.id;
+  try{const id = req.params.id;
 
   const user = await userService.findByIdService(id);
 
@@ -23,7 +25,9 @@ const validUser = async (req, res, next) => {
   req.id = id;
   req.user = user;
 
-  next();
+  next();} catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
 module.exports = { validId, validUser };
