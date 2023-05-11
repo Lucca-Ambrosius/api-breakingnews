12e395d0a325ac8a61593d23f1dcea30c9727975
Lucca Ambrosius <ismartlucca@gmail.com>
2023-05-11 04:52:46 -0300

    exporting to module

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const connectDatabase = () => {
-    console.log("Waiting for connection...")
+  console.log("Waiting for connection...");
 
-    mongoose.connect("mongodb+srv://luccaambrosiuswk:12312344@cluster0.aitu9fc.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("MongoDB atlas sucessfully connected")).catch((error) => console.log(error))
+  mongoose
+    .connect(
+      "mongodb+srv://luccaambrosiuswk:12312344@cluster0.aitu9fc.mongodb.net/?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("MongoDB atlas sucessfully connected"))
+    .catch((error) => console.log(error));
 };
 
-module.exports = connectDatabase;
+export default connectDatabase;
