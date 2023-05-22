9c744a2fae5cbe29fe8856d0a99f8c7f5368b9fa
Lucca Ambrosius <ismartlucca@gmail.com>
2023-05-22 19:30:22 -0300

    findgetall in news route

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -14,7 +14,7 @@ const create = async (req, res) => {
       title,
       text,
       banner,
-      id: "objectidfake1",
+      user: { _id: "645b4e9574deec8ee3538046" },
     });
 
     res.send(201);
@@ -23,9 +23,12 @@ const create = async (req, res) => {
   }
 };
 
-const findAll = (req, res) => {
-  const news = [];
+const findAll = async (req, res) => {
+  const news = await findAllService();
+  if (news.length === 0) {
+    return res.status(400).send({ message: "There are no registered news" });
+  }
   res.send(news);
 };
 
-export default { create, findAll };
+export { create, findAll };
